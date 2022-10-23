5980db3f34469c08deb2def43603c1d11f8b4402
bobabarbanel <bob.abarbanel@gmail.com>
2022-10-23 10:43:58 -0700

    Turn off log, DEBUG = false

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const md5 = require('md5');
-const DEBUG = true;
+const DEBUG = false;
 function log(...args) {
   if (DEBUG) {
     console.log(...args);
